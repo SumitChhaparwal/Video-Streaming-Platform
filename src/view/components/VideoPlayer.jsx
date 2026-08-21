3283e6535843c74ac9c5d31364055a7ded86932e
SumitChaparwal <chhaparwalsumit33@gmail.com>
2026-08-22 04:41:36 +0530

    Video Player Updated

--- src/view/components/VideoPlayer.jsx
+++ src/view/components/VideoPlayer.jsx
@@ -6,10 +6,12 @@ import { FaCheckCircle } from "react-icons/fa";
 import YoutubePlayer from "./YoutubePlayer";
 import { useParams } from "react-router-dom";
 import { vidData } from "../utilities/vidData";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { LiaEdit } from "react-icons/lia";
 import { TiUserDeleteOutline } from "react-icons/ti";
+import { useSelector } from "react-redux";
+import { useLocation, matchPath } from "react-router-dom";
 
 const VideoPlayer = () => {
   const { id } = useParams();
@@ -113,12 +115,19 @@ const VideoPlayer = () => {
   //recommendation vid filter
   const filteredVidData = vidData.filter((item) => item.videoId != id);
 
+   const urlLocation = useLocation();
+
+   const custFirstSidebar =
+    matchPath({ path: "/videoplayer/:id" }, urlLocation.pathname) !== null;
+
+  let menuChange = custFirstSidebar ? useSelector(store => store.home.menuChange) : false;
+
   return (
     <>
       <div className="contain flex flex-row md:mb-10 max-md:flex-col max-md:mx-auto max-md:px-2">
         <div className="sec-1 flex flex-col gap-2 ml-2 max-md:ml-0 xl:w-[75vw] lg:w-[70vw] md:w-[70vw] max-sm:w-full mt-20">
-          <div className="relative aspect-video overflow-hidden rounded-xl shadow-lg bg-black overflow-y-hidden">
-            <YoutubePlayer url={vidObj.videoLink} />
+          <div className={`relative aspect-video overflow-hidden rounded-xl shadow-lg bg-black overflow-y-hidden ${menuChange ? `-z-10`: ``}`} >
+            <YoutubePlayer url={vidObj.videoLink}/>
           </div>
           <div className="title text-lg lg:text-2xl lg:mt-1 md:text-xl font-medium line-clamp-2">
             {vidObj.title}
